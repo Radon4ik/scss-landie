4041ef11e43620a7b985e1bcc901d0a7357648d3
Radon4ik <Loboda.Radimir@gmail.com>
2022-06-21 18:07:28 +0300

    ad

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
 import './index.scss';
 
-async function check() {
-    await fetch('https://google.com');
-
-}
-
-check().then(() => {
-    console.log("success");
-}).catch(() => {
-    console.log('error');
-})
